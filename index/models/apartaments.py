34a67123c298027413e13bd887c6887449d15a7f
Gleb <nevgleb@gmail.com>
2023-12-27 15:23:49 +0300

    adding apartaments models

--- index/models/apartaments.py
+++ index/models/apartaments.py
@@ -11,7 +11,53 @@ class Apartament(models.Model):
         verbose_name="Кол-во спальных мест")
     daily_price = models.FloatField(
         default=0.0, verbose_name="Суточная стоимость (BYN)")
+    room_count = models.PositiveSmallIntegerField(
+        verbose_name="Количество комнат")
+
     slug = models.SlugField(verbose_name="Псевдним для url ссылки")
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name = "Наполнение страницы с апартаментами"
+        verbose_name_plural = "Наполнение страницы с апартаментами"
+
+
+class ApartamentPriceList(models.Model):
+    apartament = models.ForeignKey(
+        Apartament, on_delete=models.PROTECT, verbose_name="Апартаменты")
+    name = models.CharField(max_length=255, verbose_name="Наименование услуги")
+
+    class Meta:
+        verbose_name = "В стоимость входит"
+        verbose_name_plural = "В стоимость входит"
+
+
+class ApartamentConvenience(models.Model):
+    apartament = models.ForeignKey(
+        Apartament, on_delete=models.PROTECT, verbose_name="Апартаменты")
+    name = models.CharField(max_length=255, verbose_name="Удобства")
+
+    class Meta:
+        verbose_name = "Удобства"
+        verbose_name_plural = "Удобства"
+
+
+class ApartamentPhotoGalery(models.Model):
+    apartament = models.ForeignKey(
+        Apartament, on_delete=models.PROTECT, verbose_name="Апартаменты")
+    photo = models.ImageField(
+        upload_to=f"apartaments/{apartament.name}", verbose_name="Фото")
+
+
+class ApartamentMenu(models.Model):
+    apartament = models.ForeignKey(
+        Apartament, on_delete=models.PROTECT, verbose_name="Апартаменты")
+    apartament_description = models.TextField(
+        verbose_name="Описание апартаментов")
+    apartament_main_photo = models.ImageField(verbose_name="Главное фото")
+
+    class Meta:
+        verbose_name = "Наполнение для главной страницы"
+        verbose_name_plural = "Наполнение для главной страницы"
