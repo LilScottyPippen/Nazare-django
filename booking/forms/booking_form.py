306f7be21d31400279baa83218051165b81e8f85
lilscottypippen <lilscottypippen33@gmail.com>
2023-12-29 11:53:41 +0300

    Added validation of forms

--- booking/forms/booking_form.py
+++ booking/forms/booking_form.py
@@ -1,6 +1,9 @@
 from django import forms
 from ..models.booking import Booking
 from ..models.guest import Guest
+from utils.is_valid_date import *
+from utils.is_valid_full_name import *
+from utils.is_valid_phone import *
 
 
 class GuestsForm(forms.ModelForm):
@@ -13,3 +16,21 @@ class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
         fields = '__all__'
+
+    def clean(self):
+        cleaned_data = super().clean()
+        check_in_date = cleaned_data.get('check_in_date')
+        check_out_date = cleaned_data.get('check_out_date')
+        client_name = cleaned_data.get('client_name')
+        client_surname = cleaned_data.get('client_surname')
+        client_father_name = cleaned_data.get('client_father_name')
+        client_number = cleaned_data.get('client_number')
+
+        if not is_valid_date_booking(check_in_date, check_out_date):
+            raise forms.ValidationError("Booking date is invalid")
+
+        if not is_valid_full_name(client_name, client_surname, client_father_name):
+            raise forms.ValidationError("Full name is invalid")
+
+        if not is_valid_phone(client_number):
+            raise forms.ValidationError("Phone number is invalid")
--- booking/views.py
+++ booking/views.py
@@ -1,10 +1,24 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
 from django.shortcuts import render
 from django.views import View
 from django.views.generic import TemplateView
-from django.forms import formset_factory
-
+from django.forms import formset_factory, BaseFormSet
+from utils.is_valid_phone import *
+from utils.is_valid_date import *
+from utils.is_valid_full_name import *
+from .models.booking import *
 from .forms.booking_form import *
+
+
+class BaseArticleFormSet(BaseFormSet):
+    def clean(self):
+        for form in self.forms:
+            guest_name = form.cleaned_data.get('guest_name')
+            guest_second_name = form.cleaned_data.get('guest_second_name')
+            guest_father_name = form.cleaned_data.get('guest_father_name')
+
+            if not is_valid_full_name(guest_name, guest_second_name, guest_father_name):
+                raise forms.ValidationError("Full name is invalid")
 
 
 class BookingView(TemplateView):
@@ -18,20 +32,23 @@ class BookingView(TemplateView):
     def post(self, request):
         booking_form = BookingForm(request.POST)
 
-        GuestFormSet = formset_factory(GuestsForm)
+        GuestFormSet = formset_factory(GuestsForm, formset=BaseArticleFormSet)
         formset = GuestFormSet(request.POST)
 
         if booking_form.is_valid() and formset.is_valid():
             booking_instance = booking_form.save()
 
+            if booking_instance.payment_method == PAYMENT_METHOD_CHOICES[1][0]:
+                print("It's payment method is offline")
+
             for guest_form in formset:
-                guest_form.save(commit=False)
                 guest_instance = Guest.objects.create(**guest_form.cleaned_data)
                 guest_instance.booking_id = booking_instance
                 guest_instance.save()
-            return HttpResponse(request, status=200)
+
+            return HttpResponse("Success", 200)
         else:
-            return render(request, 'booking/booking.html', {'forms': formset})
+            return HttpResponseBadRequest("Form is invalid", 400)
 
 
 class GuestView(View):
@@ -39,6 +56,7 @@ class GuestView(View):
         form_count = int(request.GET.get('form-count'))
         if form_count is None:
             form_count = 1
+
         GuestFormSet = formset_factory(GuestsForm, extra=form_count)
         formset = GuestFormSet()
 
