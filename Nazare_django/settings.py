7aabf7c96979dc8d81f73280daa54c626791e2c8
lilscottypippen <lilscottypippen33@gmail.com>
2024-01-03 11:03:46 +0300

    Updated project

--- Nazare_django/settings.py
+++ Nazare_django/settings.py
@@ -121,3 +121,11 @@ ADMIN_REORDER = (
         )
     },
 )
+
+
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') == 'True'
+EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL') == 'True'
